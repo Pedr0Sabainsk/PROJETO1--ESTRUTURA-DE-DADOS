7f5bc15c62a63eefbffcfd809b1de76b2947d85a
Pedro Sabainsk <pedrosabainsk34@gmail.com>
2026-03-09 13:38:08 +0000

    criação básica da classe histórico em relação ao arraylist do chamado

--- Historico.java
+++ Historico.java
@@ -1,5 +1,13 @@
 import java.util.ArrayList;
 
 public class Historico{
-    
+    private ArrayList<Chamado> chamado;
+
+    public Pedido(){
+        this.Cadastro = new ArrayList<>();
+    }
+
+    public void cadastraCliente(Chamado chamado){
+        this.chamado.add (chamado);
+    }
 }
